810308d7d3a5a5dae2930ec6ace86a58b663693c
Bruno Degaspare <bruno@sitebrunodias.com>
2016-05-03 21:40:02 +0100

    remove comments

--- js/flickr.js
+++ js/flickr.js
@@ -4,13 +4,7 @@ $('button').click(function() {
   $(this).addClass('selected');
 
   var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  // ?jsoncallback=? this is a query that tells Flickr we are making a JSONp request
-  // the question mark (?) at the end of the URL lets you get around the security limitation
 
-  // we need to send different data depending on what kind of button is clicked (Dog, Cat, Moose)
-  // the .text() method gets all of the text inside the HTML element
-  // For example: When the Dog button is clicked, the variable animalType will hold the value of "Dog" and so on.
-  // We need to send this word to Flickr, to tell it's server which pictures we want
   var animalType = $(this).text();
 
   // flickrOptions: jQuery expects the data to be in the format of a JavaScript object
@@ -19,9 +13,6 @@ $('button').click(function() {
     format: "json"
   };
 
-  // when Flicker returns its list of photos to the page, this function displayPhotos will run
-  // the data argument represents the JSON data returned by jQuery (you can name the argument the way you want)
-  // items is the name of the object returned by FLickr's JSON feed
   function displayPhotos(data) {
     var photoHTML = '<ul>';
     // loop through each photo returned by the FLickr feed
@@ -35,9 +26,5 @@ $('button').click(function() {
   }
 
   $.getJSON(flickrAPI, flickrOptions, displayPhotos);
-  // the basic structure of jQuery's getJSON() method.
-  // It takes three arguments:
-  // First: The URL to the resource (in this case that's the Flickr's public photo feed)
-  // Second: The data we want to send along with the URL (this data will affect what information Flickr will return it)
-  // Third: This is the callback function
+
 });
